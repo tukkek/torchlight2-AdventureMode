7f9404217690eef6ed850cf62f084d6d28e630d7
Alex Henry <tukkek@gmail.com>
2021-06-02 03:14:59 -0300

    Instead of mkdirs, have the folder hierarchy ready to be copied from static/media/

--- generate.sh
+++ generate.sh
@@ -1,7 +1,5 @@
 #!/bin/bash 
 #TODO would be great to make this cross-platform but since generate.py depends on dos2unix right now, this is more elegantly done in BASh:
 rm -rf media/
-mkdir -p media/dungeons
-mkdir -p media/units/items/maps
-cp -r static/media/* media/
+cp -r static/media media/
 python3 generate.py
