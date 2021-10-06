cf15a9478812794c1851f5ed94c887bc2b528d0b
Alex Henry <tukkek@gmail.com>
2021-10-06 16:28:35 -0300

    Version will full maps, pending more extensive playtesting

--- generate.py
+++ generate.py
@@ -92,11 +92,6 @@ class ReplaceGuid(Replace):
     self.pattern='<STRING>UNIT_GUID:'
     self.replacement=f'\t<STRING>UNIT_GUID:{hash(name)}\n'
     
-class ReplaceIsMap(Replace):
-  def __init__(self):
-    self.pattern='<BOOL>MAP:'
-    self.replacement=False
-    
 class ReplaceValue(Replace):
   def __init__(self,tier):
     self.pattern='<INTEGER>VALUE:'
@@ -127,6 +122,23 @@ class ReplaceIcon(Replace):
     self.pattern='<STRING>ICON:'
     self.replacement=f'\t<STRING>ICON:{icon}_{tier.tier}\n'
 
+'''TODO
+this does not work as intended, if you enter a map scroll portal a second time, it will be as it was, see http://torchmodders.com/forums/modding-questions/weird-guts-editor-bugs/ ( https://archive.ph/wip/BdA0v )
+thankfully the chance of a repeat scroll of the same tier is less than 1% for each map generated in that tier and i've never had it happen while playtesting
+apparently you could make every strata in a dungeon 'don't store" and that will repopulate (but not re-generate) but the drawback from that is that you also can't teleport back into a dungeon after using a town portal scroll
+i imagine that ideally this should be covered by "is map" in the sense that it'd regenerate on accessing it but not opening a portal to return? alas, if there's no documentation and VOLATILE has no effect, there's not much that can be done
+'''
+class ReplaceVolatile(Replace):
+  def __init__(self):
+    self.pattern='<BOOL>VOLATILE:'
+    self.replacement=False
+
+class ReplaceIsMap(Replace):#removed with False then added in makedungeons
+  def __init__(self):
+    self.pattern='<BOOL>MAP:'
+    self.replacement=False
+    
+    
 NUMERALS=['I','II','III','IV','V','VI','VII','VIII','IX',
           'X','XI','XII','XIII','XIV','XV','XVI']
 TIERS=len(NUMERALS)
@@ -157,7 +169,7 @@ tiers=[Tier(i) for i in range(0,TIERS)]
 #use scanmaps.py to help generate maps
 maps=[Dungeon('Defiled Cemetery','map_catacombs_b_66'),Dungeon('Estherian Depths','map_estherian_a'),Dungeon('Frostbitten Caverns','map_icecaves_a'),Dungeon('Defiled Burrow','map_catacombs_2'),Dungeon('Ghastly Vault','map_catacombs_3'),Dungeon('Infernal Necropolis','map_catacombs_a_105'),Dungeon('Cursed Tombs','map_catacombs_a_56'),Dungeon('Cursed Mausoleum','map_catacombs_a_66'),Dungeon('Infernal Catacombs','map_catacombs_a_76'),Dungeon('Infernal Tombs','map_catacombs_a_86'),Dungeon('Infernal Mausoleum','map_catacombs_a_96'),Dungeon('Desecrated Sepulcher','map_catacombs_b_105'),Dungeon('Defiled Graves','map_catacombs_b_56'),Dungeon('Desecrated Burrow','map_catacombs_b_76'),Dungeon('Desecrated Graves','map_catacombs_b_86'),Dungeon('Desecrated Cemetery','map_catacombs_b_96'),Dungeon('Bloody Boneyard','map_catacombs_c_105'),Dungeon('Ghastly Crypt','map_catacombs_c_56'),Dungeon('Ghastly Mortuary','map_catacombs_c_66'),Dungeon('Bloody Vault','map_catacombs_c_76'),Dungeon('Bloody Crypt','map_catacombs_c_86'),Dungeon('Bloody Mortuary','map_catacombs_c_96'),Dungeon('Shadowy Grotto','map_caves_a'),Dungeon('Abyssal Fissure','map_caves_a_105'),Dungeon('Shadowy Pit','map_caves_a_56'),Dungeon('Shadowy Caves','map_caves_a_66'),Dungeon('Abyssal Grotto','map_caves_a_76'),Dungeon('Abyssal Pit','map_caves_a_86'),Dungeon('Abyssal Caves','map_caves_a_96'),Dungeon('Wyvern Keep','map_dragon_a'),Dungeon('Dragon Bastion','map_dragon_a105'),Dungeon('Wyvern Stronghold','map_dragon_a56-65'),Dungeon('Wyvern Citadel','map_dragon_a66-75'),Dungeon('Dragon Keep','map_dragon_a76-85'),Dungeon('Dragon Stronghold','map_dragon_a86-95'),Dungeon('Dragon Citadel','map_dragon_a96-105'),Dungeon('Forgotten Labs','map_dwarvenlabs_a'),Dungeon('Deserted Foundry','map_dwarvenlabs_a_105'),Dungeon('Forgotten Workshop','map_dwarvenlabs_a_56'),Dungeon('Forgotten Factory','map_dwarvenlabs_a_66'),Dungeon('Deserted Labs','map_dwarvenlabs_a_76'),Dungeon('Deserted Workshop','map_dwarvenlabs_a_86'),Dungeon('Deserted Factory','map_dwarvenlabs_a_96'),Dungeon('Ruined Shrine','map_estherian_b'),Dungeon('Desolate Chantry','map_estherian_b105'),Dungeon('Ruined Sanctuary','map_estherian_b56'),Dungeon('Ruined Temple','map_estherian_b66'),Dungeon('Desolate Shrine','map_estherian_b76'),Dungeon('Desolate Sanctuary','map_estherian_b86'),Dungeon('Desolate Temple','map_estherian_b96'),Dungeon('Infected Hollow','map_estherian_c'),Dungeon('Blighted Sanctum','map_estherian_c_105'),Dungeon('Infected Retreat','map_estherian_c_56'),Dungeon('Infected Depths','map_estherian_c_66'),Dungeon('Blighted Hollow','map_estherian_c_76'),Dungeon('Blighted Retreat','map_estherian_c_86'),Dungeon('Blighted Depths','map_estherian_c_96'),Dungeon('Frostshorn Breach','map_icecaves_a_105'),Dungeon('Frostbitten Ravine','map_icecaves_a_56'),Dungeon('Frostbitten Chasm','map_icecaves_a_66'),Dungeon('Frostshorn Caverns','map_icecaves_a_76'),Dungeon('Frostshorn Ravine','map_icecaves_a_86'),Dungeon('Frostshorn Chasm','map_icecaves_a_96'),Dungeon('Ransacked Halls','map_vaults_a'),Dungeon('Ezrohir Treasury','map_vaults_a105'),Dungeon('Ransacked Commons','map_vaults_a56'),Dungeon('Ransacked Vault','map_vaults_a66'),Dungeon('Ezrohir Halls','map_vaults_a76'),Dungeon('Ezrohir Commons','map_vaults_a86'),Dungeon('Ezrohir Vault','map_vaults_a96'),Dungeon('Cursed Catacombs','maproom_catacombs_1')]
 dungeons=[Dungeon("Banepits","A3-banepits"),Dungeon("Cellar of Madness","a3werewolfcellar"),Dungeon("Cursed Fear","A2Z1_CURSEDFEAR_DESERTCAVES"),Dungeon("Desert Caves","desertcaves"),Dungeon("Northwestern Ice Lab","NG_NW_ICELABS"),Dungeon("Southeastern Ice Lab","NG_SE_ICELABS"),Dungeon("Witherways","witherways")]
-bosses=[Dungeon('3 sisters','A3-3SISTERS'),Dungeon('Arena of Slaughter','arenaofslaughter'),Dungeon("Cacklespit's Realm","cacklespitsrealm"),Dungeon("Drigung Grotto","cultistslair"),Dungeon("Dead Shoals","deadshoals"),Dungeon("Jehannum","jehannum"),Dungeon("Korari Cave","koraricave"),Dungeon("Mana Vent","manavent"),Dungeon("Bear Cave","ngbearcave"),Dungeon("Warforge Armory","ngdwarfarmory"),Dungeon("Pirates Cove","piratecove"),Dungeon("Notch's Mine","a3quarrymine"),Dungeon("Rift Keep","riftkeep"),Dungeon("Swa Mill","thesawmill"),Dungeon("Scrapworks","a3-scrapworks"),Dungeon("Slavers den","slaversden"),Dungeon("Stygian Aerie","manticorelair"),Dungeon("Swarm Point","swarmpoint"),Dungeon("Tarroch's Tomb","desertcatacombs"),Dungeon("The Emberscratch Mines","GOBLIN_EMBERCAVES"),Dungeon("The Garden of Tears","thegardenoftears"),Dungeon("Thing","thing"),Dungeon("Tower of the Moon","towerofthemoon"),Dungeon("Undercurrents","undercurrents"),Dungeon("Vault of Souls","vaultofsouls"),Dungeon("Warforge","a3-warforge"),Dungeon("Wellspring Treasury","wellspringtreasury"),Dungeon("Widow's Veil","crowspass_spidercave")]
+bosses=[Dungeon('3 sisters','A3-3SISTERS'),Dungeon('Arena of Slaughter','arenaofslaughter'),Dungeon("Cacklespit's Realm","cacklespitsrealm"),Dungeon("Drigung Grotto","cultistslair"),Dungeon("Dead Shoals","deadshoals"),Dungeon("Jehannum","jehannum"),Dungeon("Korari Cave","koraricave"),Dungeon("Mana Vent","manavent"),Dungeon("Bear Cave","ngbearcave"),Dungeon("Warforge Armory","ngdwarfarmory"),Dungeon("Pirates Cove","piratecove"),Dungeon("Notch's Mine","a3quarrymine"),Dungeon("Rift Keep","riftkeep"),Dungeon("Saw Mill","thesawmill"),Dungeon("Scrapworks","a3-scrapworks"),Dungeon("Slavers den","slaversden"),Dungeon("Stygian Aerie","manticorelair"),Dungeon("Swarm Point","swarmpoint"),Dungeon("Tarroch's Tomb","desertcatacombs"),Dungeon("The Emberscratch Mines","GOBLIN_EMBERCAVES"),Dungeon("The Garden of Tears","thegardenoftears"),Dungeon("Thing","thing"),Dungeon("Tower of the Moon","towerofthemoon"),Dungeon("Undercurrents","undercurrents"),Dungeon("Vault of Souls","vaultofsouls"),Dungeon("Warforge","a3-warforge"),Dungeon("Wellspring Treasury","wellspringtreasury"),Dungeon("Widow's Veil","crowspass_spidercave")]
 wilds=[Dungeon("Battlefield","a3-battlefield"),Dungeon("Blight Bogs","a3blightbogs"),Dungeon("Brood Hive","broodhive"),Dungeon("Crows pass","crowspass"),Dungeon("Dank Hollow","a3pass1"),Dungeon("Frosted Hills","frostedhills"),Dungeon("Haunted Quarter","hauntedquarter"),Dungeon("Ossean Wastes","osseanwastes"),Dungeon("Path of the Honored Dead","pathofhonoreddead"),Dungeon("Rivenskull Gorge","a3pass2"),Dungeon("Salt Barrens","saltbarrens"),Dungeon("The Temple Steppes","templesteppes"),Dungeon("Vulture Pass","vulturepass")]
 nether=[Dungeon("Netherrealm","nether"),Dungeon("Netherrealm","nether_a1z1"),Dungeon("Netherrealm","nether_a1z2"),Dungeon("Netherrealm","nether_a2z1"),Dungeon("Netherrealm","nether_a2z2"),Dungeon("Netherrealm","nether_a3z1"),Dungeon("Netherrealm","nether_a3z2")]
 challenges=[Dungeon("Challenge","phasebeast_a1z1_all"),Dungeon("Challenge","phasebeast_a1z2_all"),Dungeon("Challenge","phasebeast_a2z1_all"),Dungeon("Challenge","phasebeast_a2z2_all"),Dungeon("Challenge","phasebeast_a2z2_JT"),Dungeon("Challenge","phasebeast_a3z1_all"),Dungeon("Challenge","phasebeast_a3z2_all"),Dungeon("Challenge","phasebeast_a3z2_lava"),Dungeon("Challenge","phasepillar"),Dungeon("Luminous Arena","luminousarena")]
@@ -199,14 +211,15 @@ def makedungeons(maps,icon):
   for m in maps:
     yield m
     for t in tiers:
-      dungeonname=f'am_{m.dungeonname}'
+      name=f'{m.dungeonname}_{t.tier+1}'
+      dungeonname=f'am_{name}'
       r=[ReplaceDisplayName(f'{m.name} (Tier {t.name})'),
         ReplaceName(dungeonname),ReplaceParentDungeon(),
         ReplaceParentTown(),ReplaceMinMatchLevel(t),
         ReplaceMaxMatchLevel(t),ReplaceIsMap()]
-      a=[f'\t<INTEGER>PLAYER_LVL_MATCH_OFFSET:{t.offset}\n']
+      a=[f'\t<INTEGER>PLAYER_LVL_MATCH_OFFSET:{t.offset}\n','\t<BOOL>MAP:true\n']
       modify(m.dungeon,dungeonname,replace=r,add=a)
-      mapname=f'am_map_{m.dungeonname}'
+      mapname=f'am_map_{name}'
       r=[ReplaceDisplayName(f'{m.name} map ({t.name})'),
         ReplaceName(mapname),ReplaceDescription(m,t),
         ReplaceRarity(t),ReplaceDungeon(dungeonname),
