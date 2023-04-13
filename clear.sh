1a74e0a97631920fe6ea96e03a0b73cfb315b325
Alex Henry <tukkek@gmail.com>
2023-04-13 15:45:13 -0300

    Versioning-related utility scripts

---
+++ clear.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+set -e
+
+rm media/dungeons/*.dat media/units/items/maps/*.dat
---
+++ restore.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+set -e
+
+git restore media/dungeons/*.dat media/units/items/maps/*.dat
+git status
