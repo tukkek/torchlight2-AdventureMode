10ef2fe0f5ee7d7b92daba30dce22eecf801454c
Alex Henry <tukkek@gmail.com>
2023-04-14 16:17:53 -0300

    Copying files rather than folders to avoid permission issues

--- copy.sh
+++ copy.sh
@@ -2,15 +2,15 @@
 set -e
 
 echo Clearing GUTS data
-nice rm -rf 'guts/Adventure mode/media/dungeons/'
-nice rm -rf 'guts/Adventure mode/media/units/items/maps/'
-nice rm -rf 'guts/Adventure mode/media/spawnclasses/'
+nice rm -f 'guts/Adventure mode/media/dungeons/'*
+nice rm -f 'guts/Adventure mode/media/units/items/maps/'*
+nice rm -f 'guts/Adventure mode/media/spawnclasses/'*
 
 echo Copy dungeons
-nice gcp -r 'media/dungeons/' 'guts/Adventure mode/media/dungeons/'
+nice gcp -r 'media/dungeons/'* 'guts/Adventure mode/media/dungeons/'
 
 echo Copy maps
-nice gcp -r 'media/units/items/maps/' 'guts/Adventure mode/media/units/items/maps/'
+nice gcp -r 'media/units/items/maps/'* 'guts/Adventure mode/media/units/items/maps/'
 
 echo Copy spawnclasses
-nice gcp -r 'static/media/spawnclasses/' 'guts/Adventure mode/media/spawnclasses/'
+nice gcp -r 'static/media/spawnclasses/'* 'guts/Adventure mode/media/spawnclasses/'
