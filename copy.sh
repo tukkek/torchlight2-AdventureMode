d8ef6ea6181d0800ac94f124af0185d25c3da645
Alex Henry <tukkek@gmail.com>
2023-04-25 00:00:00 +0000

    Copying entire folders and setting permissions...
    BASH was not too pleased with copying 44,400 files as command-line arguments *shrug*

--- copy.sh
+++ copy.sh
@@ -3,16 +3,19 @@ set -e
 
 echo For development and testing only!
 
-echo Clearing GUTS data
-nice rm -f 'guts/Adventure mode/media/dungeons/'*
-nice rm -f 'guts/Adventure mode/media/units/items/maps/'*
-nice rm -f 'guts/Adventure mode/media/spawnclasses/'*
+echo Clearing GUTS data...
+nice rm -rf 'guts/Adventure mode/media/dungeons/'
+nice rm -rf 'guts/Adventure mode/media/units/items/maps/'
+nice rm -rf 'guts/Adventure mode/media/spawnclasses/'
 
-echo Copy dungeons
-nice gcp -r 'media/dungeons/'* 'guts/Adventure mode/media/dungeons/'
+echo Copy dungeons...
+nice gcp -r 'media/dungeons/' 'guts/Adventure mode/media/'
 
-echo Copy maps
-nice gcp -r 'media/units/items/maps/'* 'guts/Adventure mode/media/units/items/maps/'
+echo Copy maps...
+nice gcp -r 'media/units/' 'guts/Adventure mode/media/'
 
-echo Copy spawnclasses
-nice gcp -r 'static/media/spawnclasses/'* 'guts/Adventure mode/media/spawnclasses/'
+echo Copy spawnclasses...
+nice gcp -r 'static/media/spawnclasses/' 'guts/Adventure mode/media/'
+
+echo Setting permissions...
+nice chmod -R 0777 'guts/Adventure mode/media/'
