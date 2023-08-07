af76d34adf7f5ede6eb5701bfc6dad4d2414b8df
Alex Henry <tukkek@gmail.com>
2023-08-07 00:00:00 +0000

    Restructuring copying helper script...
    Mostly so it's easier to copy-paste sections into the terminal.

--- copy.sh
+++ copy.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/bash
+#TODO turn into makefile?
 set -e
 
 echo For development and testing only!
 
-echo Clearing GUTS data...
-nice rm -rf 'guts/Adventure mode/media/dungeons/'
-nice rm -rf 'guts/Adventure mode/media/units/'
-nice rm -rf 'guts/Adventure mode/media/spawnclasses/'
-
 echo Copy dungeons...
+nice rm -rf 'guts/Adventure mode/media/dungeons/'
 nice gcp -r 'media/dungeons/' 'guts/Adventure mode/media/'
+nice chmod -R 0777 'guts/Adventure mode/media/dungeons/'
 
 echo Copy units...
+nice rm -rf 'guts/Adventure mode/media/units/'
 nice gcp -r 'media/units/' 'guts/Adventure mode/media/'
+nice chmod -R 0777 'guts/Adventure mode/media/units/'
 
 echo Copy spawnclasses...
+nice rm -rf 'guts/Adventure mode/media/spawnclasses/'
 nice gcp -r 'static/media/spawnclasses/' 'guts/Adventure mode/media/'
-
-echo Setting permissions...
-nice chmod -R 0777 'guts/Adventure mode/media/dungeons/' 'guts/Adventure mode/media/units/' 'guts/Adventure mode/media/spawnclasses/'
+nice chmod -R 0777 'guts/Adventure mode/media/spawnclasses/'
