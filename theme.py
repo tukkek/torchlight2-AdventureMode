8a722d9945c1d626015c44cccb478cf19bd7bd62
Alex Henry <tukkek@gmail.com>
2023-08-07 00:00:00 +0000

    Generating affix themes #13

--- theme.py
+++ theme.py
@@ -1,3 +1,24 @@
+import random
+
+MIN='\t<INTEGER>MINRANDOMAFFIXES:{}'
+MAX='\t<INTEGER>MAXRANDOMAFFIXES:{}'
+AFFIX='	\t<STRING>AFFIX:{}'
+PLAYERBUFFS='''
+\t[RANDOMPLAYERAFFIXES]
+{}
+\t[/RANDOMPLAYERAFFIXES]
+'''
+MONSTERBUFFS='''
+\t[RANDOMMONSTERAFFIXES]
+{}
+\t[/RANDOMMONSTERAFFIXES]
+'''
+MONSTERDEBUFFS='''
+\t[RANDOMMONSTERPENALTYAFFIXES]
+{}
+\t[/RANDOMMONSTERPENALTYAFFIXES]
+'''
+
 #!/usr/bin/python3
 class Affixes:
   def __init__(self):
@@ -14,9 +35,9 @@ class Theme:
   def count(self):
     return sum(len(affixes) for affixes in [self.player.buffs,self.monster.buffs,self.monster.debuffs])
 
-class Infernal(Theme):
+class Blazing(Theme):
   def __init__(self):
-    super().__init__('Infernal')
+    super().__init__('Blazing')
     self.player.buffs=['MAP_BURNING','MAP_FIRE_DAMAGE_PERCENT_BONUS_15'] 
     self.monster.buffs=['MAP_BURNING','MAP_EXPLODEONDEATH']
 
@@ -68,10 +89,56 @@ class Toxic(Theme):
     self.player.buffs=['MAP_POISON_DAMAGE_PERCENT_BONUS_15']
     self.monster.buffs=['MAP_NOXIOUS','MAP_POISON_DAMAGE_PERCENT_BONUS_15']
 
-themes=[t() for t in [Infernal,Galvanic,Glacial,Brutal,Vigorous,Lucky,Arcane,Warped,Toxic]]
+themes=[t() for t in [Blazing,Galvanic,Glacial,Brutal,Vigorous,Lucky,Arcane,Warped,Toxic]]
 
-count=0
-for t in themes:
-  print(t,t.count())
-  count+=t.count()
-print(count)
+def clean(line):
+  if 'MINRANDOMAFFIXES' in line or 'MAXRANDOMAFFIXES' in line:
+    return True
+  if 'RANDOM' in line and 'AFFIXES' in line:
+    return True
+  if '<STRING>AFFIX:' in line:
+    return True
+  return False
+
+def splice(line,lines):
+  lines.insert(len(lines)-1,line+'\n')
+
+def add(affixes,template,lines):
+  if len(affixes)==0:
+    return
+  affixes='\n'.join(AFFIX.format(a) for a in affixes)
+  template=template.format(affixes)
+  for line in template.split('\n'):
+    if len(line.strip())>0:
+      splice(line,lines)
+
+'''
+Affixes aren't the same in the actual dungeon as on the scroll.
+This is probably because of using an item effect rather than mapworks prop?
+Fix it by deciding affixes here at generation time.
+There are tens of thousands of scrolls so it's virtually random either way.
+Vast majority of scrolls are 0-4 affixes, so using that here too.
+'''
+def generate(filename,lines):
+  scroll=False
+  for i in reversed(range(len(lines))):
+    if clean(lines[i]):
+      lines.pop(i)
+      scroll=True
+  if not scroll:
+    return
+  random.seed(filename)
+  t=random.choice(themes)
+  for i in range(len(lines)):
+    l=lines[i]
+    if 'DISPLAYNAME:' in l:
+      lines[i]=l.replace('DISPLAYNAME:',f'DISPLAYNAME:{t.name} ')
+      break
+  c=t.count()
+  splice(MIN.format(c),lines)
+  splice(MAX.format(c),lines)
+  add(t.player.buffs,PLAYERBUFFS,lines)
+  m=t.monster
+  add(m.buffs,MONSTERBUFFS,lines)
+  add(m.debuffs,MONSTERDEBUFFS,lines)
+  
