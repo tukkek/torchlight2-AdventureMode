42996805028592dcab715325987cedabba042733
Alex Henry <tukkek@gmail.com>
2023-08-08 00:00:00 +0000

    Summary of themes

--- summary.py
+++ summary.py
@@ -1,4 +1,4 @@
-import generate,goal,textwrap
+import generate,goal,textwrap,theme
 
 TIERS=textwrap.dedent('''
   {} tiers generated for:
@@ -8,7 +8,10 @@ REWARDS=textwrap.dedent('''
   {} map reward categories, {} reward types:
   {}
 ''')
+THEMES='{} thematic sets of map modifiers ({}).'
 SUMMARY=textwrap.dedent('''
+  {}
+  
   {}
   
   {}
@@ -19,7 +22,7 @@ SUMMARY=textwrap.dedent('''
 def show(count):
   categories=sorted(generate.categories,key=lambda x:len(x.maps),reverse=True)
   tiers='\n'.join(f'- {len(c.maps)} {c.category}' for c in categories)
-  tiers=TIERS.format(len(generate.tiers),tiers)
+  tiers=TIERS.format(len(generate.tiers),tiers).strip()
   goals=goal.categories
   rewards=[]
   for g in sorted(goals,key=lambda k:len(k),reverse=True):
@@ -31,7 +34,9 @@ def show(count):
       entry=f'{ntypes} for {name} ({types})'
     rewards.append(f'- {entry}')
   ngoals=sum(len(g) for g in goals)
-  rewards=REWARDS.format(len(goals),ngoals,'\n'.join(rewards))
-  s=SUMMARY.format(tiers.strip(),rewards.strip(),f'{count:,}').strip()
+  rewards=REWARDS.format(len(goals),ngoals,'\n'.join(rewards)).strip()
+  themes=', '.join(sorted(t.name.lower() for t in theme.themes))
+  themes=THEMES.format(len(theme.themes),themes)
+  s=SUMMARY.format(tiers,rewards,themes,f'{count:,}').strip()
   print(s)
   print(s,file=open('summary.txt','w'))
